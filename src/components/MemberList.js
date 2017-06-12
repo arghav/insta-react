e3a0b56f5d80a00a600a2b51542dbb36a3143b81
Amal Raghav <amal.raghav@gmail.com>
2017-06-12 12:24:07 +0530

    Simplify list component structure

--- src/components/MemberList.js
+++ src/components/MemberList.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import './MemberList.css';
 
-import List from '../components/List'
-
 class Team extends Component {
   render() {
+    const listItems = this.props.items.map((item) =>
+      <div key={item.id} className="ListItem" onClick={() => this.props.onItemClick(item)}>
+        <div className="name">{item.firstName + ' ' + item.lastName}{ "true" === item.isAdmin && <span>(admin)</span>}</div>
+        <div className="phone">{item.phone}</div>
+        <div className="email">{item.email}</div>
+      </div>
+    );
+
     return (
       <div className="MemberList">
-        <List items={this.props.items} onItemClick={this.props.onItemClick}/>
+        {listItems}
       </div>
     );
   }
