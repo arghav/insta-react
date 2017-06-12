f4012e87250bc0982e04ea8e749d4c7b74ac8b14
Amal Raghav <amal.raghav@gmail.com>
2017-06-12 16:33:08 +0530

    Fix admin label bug

--- src/components/MemberList.js
+++ src/components/MemberList.js
@@ -5,7 +5,7 @@ class Team extends Component {
   render() {
     const listItems = this.props.items.map((item, index) => (
       <div key={index} className="ListItem" onClick={() => this.props.onItemClick(item, index)}>
-        <div className="name">{item.firstName + ' ' + item.lastName}{ "true" === item.isAdmin && <span>(admin)</span>}</div>
+        <div className="name">{item.firstName + ' ' + item.lastName}&nbsp;{ true === item.isAdmin && <span>(admin)</span>}</div>
         <div className="phone">{item.phone}</div>
         <div className="email">{item.email}</div>
       </div>
